b8197e0686774d25342ea577e8b4e03f8cd47a57
shida-zheng-ICD2O1-01 <shida.zheng@student.tdsb.on.ca>
2024-09-23 13:51:00 +0000

    a

--- PP1_5.py
+++ PP1_5.py
@@ -1,19 +1,34 @@
 
 def q1():
   #Write Assignment code here
-
+ a = input("enter a integer")
+ b = int(a) + 3
+ print(b)
 def q2():
   #Write Assignment code here
-
-def q3():
+ a = input("Enter a number:")
+ b = a + "4"
+ c = float(b) + 2
+ print(c)
+def q3(): 
   #Write Assignment code here
-
+ a = input("Enter a radius:")
+ b = float(a)
+ pi = 3.14
+ c = pi * (b ** 2)
+ print(c)
 def q4():
   #Write Assignment code here
-
+ a = input("Enter a number:")
+ b = float(a) * 12
+ c = int(b)
+ print(c)
+ 
 def q5():
   #Write Assignment code here
-
+ a = input("Enter a integer:")
+ b = int(a) + 5
+ print(f"Your number + 5 is {b}")
 #Comment this code out when running tests
 #Do not comment this out when running your program normally
 
