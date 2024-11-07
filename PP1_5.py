22f0238f919401e7baf6ce3e99ccb0b577892246
shida-zheng-ICD2O1-01 <shida.zheng@student.tdsb.on.ca>
2024-11-07 15:34:58 +0000

    1

--- PP1_5.py
+++ PP1_5.py
@@ -1,24 +1,34 @@
 
 def q1():
   #Write Assignment code here
-
+  num = int(input("Input an integer: "))
+  num += 3
+  print(num)
 def q2():
   #Write Assignment code here
-
+  num_str = input("Input a number: ")
+  num_with_4 = float(num_str + "4") + 2
+  print(num_with_4)
 def q3():
   #Write Assignment code here
-
+  radius = float(input("Input a radius: "))
+  area = 3.14 * radius ** 2
+  print(f"The area of the circle is: {area}")
 def q4():
   #Write Assignment code here
-
+  num_float = float(input("Input a number: "))
+  result = int(num_float * 12) 
+  print(result)
 def q5():
   #Write Assignment code here
-
+  integer = int(input("Input an integer: "))
+  integer += 5
+  print(f"Your number + 5 is {integer}")
 #Comment this code out when running tests
 #Do not comment this out when running your program normally
 
-q1()
-q2()
-q3()
-q4()
-q5()
+#q1()
+#q2()
+#q3()
+#q4()
+#q5()
